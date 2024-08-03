713b26afa773e468acc9b7fa1e3cde723dc8ad3a
MUKESHBAGAL <2022bit039@sggs.ac.in>
2024-08-03 23:28:38 +0530

    Practical No 3 Pattern Printing

--- 3.1Q_Solution.py
+++ 3.1Q_Solution.py
@@ -1,20 +1,18 @@
-def print_pattern():
-    for i in range(3):  # Loop over rows
-        for j in range(6):  # Loop over columns
-            if i == 0 and j == 6 // 2:
-                print("* ", end="")
-            elif i == 1 and (j >= i and j < 5 - 1):
-                if i + 1 == j:
-                    print(" ! ", end="")
+def print_pattern(n):
+    for i in range(n):
+        for j in range(2*n): 
+            if j + i >= n and j - i <= n:
+                if(i==n//2 and j==n):
+                  print("! ", end="") 
                 else:
-                    print(" * ", end="")
-            elif i == 2 and j < 6 - 1:
-                print(" * ", end="")
+                 print("* ", end="") 
             else:
-                print("  ", end="")
-        print()  # Move to the next line
+                print("  ", end="") 
+        print()
 
-print_pattern()
+
+num = int(input("Enter a number :"))        
+print_pattern(num)
 
 '''
 OUTPUT::
@@ -31,26 +29,30 @@ OUTPUT::
 """
 #include <stdio.h>
 
-int main() {
-    for(int i = 0; i < 3; i++) { // Loop over rows
-        for(int j = 0; j < 6; j++) { // Loop over columns
-            if(i == 0 && j == 6 / 2)
-                printf("* ");
-           else if(i==1&& (j>=i&&j<5-1)){
-               if(i+1==j)
-                 printf(" ! ");
-              
-               else 
-                 printf(" * ");
-           }
-            else if(i == 2 && j < 6 - 1)
-                printf(" * ");
-            else 
+void print_pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2 * n; j++) {
+            if (j + i >= n && j - i <= n) {
+                if (i == n / 2 && j == n) {
+                    printf("! ");
+                } else {
+                    printf("* ");
+                }
+            } else {
                 printf("  ");
+            }
         }
         printf("\n");
     }
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    print_pattern(num);
     return 0;
 }
+
 
 """
--- 3.2Q_Solution.py
+++ 3.2Q_Solution.py
@@ -1,38 +1,44 @@
-def print_pattern():
-    for i in range(6):  # Loop over rows
-        for j in range(7):  # Loop over columns
-            if i == 0 and j == 7 // 2:
-                print("  * ", end="")
-            elif i == 1 and j % 2 == 0 and (j > 0 and j < 7 - 1):
-                print("  * ", end="")
-            elif i == 2 and j % 2 == 1:
-                if i + 1 == j:
-                    print(" 2 ", end="")
-                else:
-                    print("  * ", end="")
-            elif i == 3 and j % 2 == 1 and (j > 1 and j < 7 - 1):
-                print(" * ", end="")
-            elif (i > 3 and i < 6) and j < 7:
-                print("  * ", end="")
+def print_pattern(n):
+    if(n<3):
+        print("Plese Enter a Greater than ",n," Input !!")
+    for i in range(2*n-2):
+        for j in range(2*n): 
+            if (j + i == n and i < n) or \
+               (j - i == n) or \
+               (i - j == n - 2 and (i > n - 1 and i <= 2*n - 3)) or \
+               (i + j == 3*n - 2 and (i > n - 1 and i <= 2*n - 3)):
+                print("* ", end="") 
+            elif i == n - 1 and j == n:
+                print(n, " ", end="") 
             else:
-                print("   ", end="")
-        print()  # Move to the next line
+                print("   ", end="") 
+        print()
 
-print_pattern()
+    
+    for i in range(n):  
+        for j in range(2*n - 1):  
+            print(" * ", end="") 
+        print() 
+
+num = int(input("Enter a number :"))        
+print_pattern(num)  
 
 '''
 OUTPUT::
 
-           *          
-        *      *       
-     *      2      *    
-          *     *    
- *   *   *   *   *   *   *  
- *   *   *   *   *   *   *    
+Enter a number :3
+
+          *         
+     *        *     
+ *        3        * 
+     *        *             
+ *  *  *  *  *  *  *  *  * 
+ *  *  *  *  *  *  *  *  * 
+ *  *  *  *  *  *  *  *  * 
+
  
  
 '''
-
 
 
 # Refrance code
@@ -40,29 +46,47 @@ OUTPUT::
 """
 #include <stdio.h>
 
-int main() {
-    for(int i = 0; i < 6; i++) { // Loop over rows
-        for(int j = 0; j < 7; j++) { // Loop over columns
-            if(i==0 &&j==7/2)
-                printf("  * ");
-            else if(i==1 && j%2==0 && (j>0 && j<7-1)) 
-                printf("  * ");
-            else if(i==2 && j%2==1 ){
-                if(i+1==j)            
-                  printf("  2 ");
-                else
-                  printf("  * ");
-            }
-            else if(i==3 && j%2==1 && (j>1 && j<7-1))
-              printf(" * ");
-            else if((i>3 &&i<6) && j<7)
-                printf(" *  ");
-            else 
+void print_pattern(int n) {
+    if (n < 3) {
+        printf("Please Enter a Number Greater than %d\n", n);
+        return;
+    }
+
+    for (int i = 0; i < 2*n - 2; i++) {
+        for (int j = 0; j < 2*n; j++) {
+            if ((j + i == n && i < n) || 
+                (j - i == n) || 
+                (i - j == n - 2 && (i > n - 1 && i <= 2*n - 3)) || 
+                (i + j == 3*n - 2 && (i > n - 1 && i <= 2*n - 3))) {
+                printf("* ");
+            } else if (i == n - 1 && j == n) {
+                printf("%d ", n);
+            } else {
                 printf("   ");
+            }
         }
         printf("\n");
     }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2*n - 1; j++) {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    print_pattern(num);
+
     return 0;
 }
+
+
 
 """
