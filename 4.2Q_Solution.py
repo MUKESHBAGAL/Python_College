913de724d7518f4638bb5e85d4616f26c657c881
MUKESHBAGAL <2022bit039@sggs.ac.in>
2024-08-07 15:33:29 +0530

    Practile No 4

---
+++ 4.2Q_Solution.py
@@ -0,0 +1,17 @@
+def print_pattern(n):
+    for i in range(2*n+1):
+        for j in range(2*n+1): 
+            if (j + i == n ) or \
+               (j - i == n):
+               	 print("+ ", end="") 
+            if (i-j==n and (i>n and i<2*n))or \
+               (i+j==3*n and (i>n and i<2*n)):
+                print(" - ", end="") 
+            elif i == 2*n and j == n:
+               	 print(" - ", end="") 
+            else:
+                print("   ", end="") 
+        print()
+
+num = int(input("Enter a number :"))        
+print_pattern(num)
